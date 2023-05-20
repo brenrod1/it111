2479f7fececf32fd3527f5b49c1505b0e69303da
brenrod1 <brenrod1@gmail.com>
2023-05-19 18:11:13 -0700

    Add files via upload

---
+++ SimpleWindow2.java
@@ -0,0 +1,10 @@
+public class SimpleWindow2 {
+
+
+    public  static void main(String[]args){
+        //this file is creating an instance of the Simple Window class
+
+        SimpleWindow window = new SimpleWindow();
+
+    }
+}
